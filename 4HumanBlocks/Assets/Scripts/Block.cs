8ae85f8c6e3184eab974071cfcaf0f469765f414
Nick <theerapat.nick@gmail.com>
2020-02-01 08:13:16 +0700

    Create block system

---
+++ 4HumanBlocks/Assets/Scripts/Block.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //  Mock
+    [ SerializeField ] float rotateSpeed = 2;
+
+    [ SerializeField ] float correctPosition = 0;
+
+    float rotateInput;
+
+
+    Vector3 blockRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        blockRotation = transform.rotation.eulerAngles;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateInput = Input.GetAxis( "Horizontal" );
+        
+        transform.Rotate( 0.0f, rotateInput * rotateSpeed, 0.0f, Space.World );
+    }
+
+    public bool isCorrectOrientation( float threshold = 5 )
+    {
+
+
+
+        Debug.Log( "transform y - " + transform.rotation.eulerAngles.y );
+
+        float currentTransformY =  transform.rotation.eulerAngles.y;
+
+        bool isOriented = correctPosition-threshold < currentTransformY &&
+                            currentTransformY < correctPosition+threshold;
+
+        return isOriented;
+    }
+
+    public string getCredential()
+    {
+
+        //mock
+        return "B01";
+    }
+
+}
---
+++ 4HumanBlocks/Assets/Scripts/BlockDetector.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDetector : MonoBehaviour
+{
+
+    [ SerializeField ] float errorThreshold = 10;
+
+    private Collider detectorCollider;
+
+    private List< Block > blockInDetector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        blockInDetector = new List< Block>();
+        
+        detectorCollider = GetComponent< Collider >();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter( Collider other )
+    {
+        Block otherBlock = other.gameObject.GetComponent< Block >();
+
+        if (otherBlock != null )
+            blockInDetector.Add( otherBlock );
+
+        Debug.Log( "blockInDector size " + blockInDetector.Count );
+        Debug.Log( otherBlock );
+
+        foreach ( Block block in blockInDetector )
+        {
+
+            Debug.Log( "    Block " + block + " is " + block.isCorrectOrientation( errorThreshold ) );
+
+        }
+
+        if ( blockInDetector.Count >= 4 )
+
+            if ( checkCombineBlock( "B01" ))
+                Debug.Log( "SUCCESS!!!");
+    }
+
+    void OnTriggerExit( Collider other )
+    {
+
+        Block otherBlock = other.gameObject.GetComponent< Block >();
+
+        blockInDetector.Remove( otherBlock );
+
+    }
+
+    bool checkCombineBlock( string requiredBlockId )
+    {
+
+        foreach( Block block in blockInDetector )
+        {
+            if ( block.getCredential() != requiredBlockId )
+                return false;
+        }
+        
+        return true;
+
+    }
+}
