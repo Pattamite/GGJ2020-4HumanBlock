67cfee0406d882f3d7cc7f789208b47d329c6540
ThipokKub <thipok30386@gmail.com>
2020-02-02 09:16:00 +0700

    Update more UI Assets

---
+++ 4HumanBlocks/Assets/Scripts/ImageUIButton.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ImageUIButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler {
+
+    public Sprite Idle;
+    public Sprite Hover;
+    public Sprite Click;
+
+    private float xRatio;
+    private float yRatio;
+    private Vector2 originalScale;
+
+    void Start () {
+        originalScale = transform.localScale;
+        xRatio = transform.localScale[0] / Idle.rect.size[0];
+        yRatio = transform.localScale[1] / Idle.rect.size[1];
+    }
+
+    Vector3 getScale (Sprite target) {
+        // new Scale
+        float xSize = xRatio * target.rect.size[0];
+        float ySize = yRatio * target.rect.size[1];
+        // Normalize new scale
+        xSize = xSize / (xSize / originalScale[0]);
+        ySize = ySize / (xSize / originalScale[0]);
+        return new Vector3 (xSize, ySize, 1.0f);
+    }
+
+    public void OnPointerEnter (PointerEventData eventData) {
+        if (Hover != null) {
+            GetComponent<Image> ().sprite = Hover;
+            transform.localScale = getScale (Hover);
+        }
+    }
+
+    public void OnPointerExit (PointerEventData eventData) {
+        GetComponent<Image> ().sprite = Idle;
+        transform.localScale = getScale (Idle);
+    }
+
+    public void OnPointerDown (PointerEventData eventData) {
+        if (Hover != null) {
+            GetComponent<Image> ().sprite = Click;
+            transform.localScale = getScale (Click);
+        }
+    }
+
+    public void OnPointerUp (PointerEventData eventData) {
+        if (Hover != null) {
+            GetComponent<Image> ().sprite = Hover;
+            transform.localScale = getScale (Hover);
+        } else {
+            GetComponent<Image> ().sprite = Idle;
+            transform.localScale = getScale (Idle);
+        }
+    }
+}
